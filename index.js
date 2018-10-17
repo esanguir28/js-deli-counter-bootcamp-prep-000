ed38188f1c5f37b4495a9cc602819bd943e93db1
Erica Janine Sanguir <ericajanisan@gmail.com>
2018-10-17 23:03:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ function takeANumber(katzDeliLine, name) {
   return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line."
 }
 
-function nowServing(katzDeliLine){
-  
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return "There is nobody waiting to be served!"
+  } else {
+    var name = katzDeliLine[0];
+    katzDeliLine.splice(0, 1);
+    return "Currently serving " + name + ".";
+  }
 }
