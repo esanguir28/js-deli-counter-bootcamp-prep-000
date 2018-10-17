855921a2403e24830ceab4c85d859313fb4cd022
Erica Janine Sanguir <ericajanisan@gmail.com>
2018-10-17 23:03:02 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,10 @@
+var katzDeliLine = [];
+
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name)
+  return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line."
+}
+
+function nowServing(katzDeliLine){
+  
+}
